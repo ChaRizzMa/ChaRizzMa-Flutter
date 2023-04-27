8c328300b65fd54e8aa2d7b62c887288d1000301
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2023-04-27 12:40:17 -0400

    migrating to supabase

---
+++ lib/Pages/Home/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/common/theme.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+// TODO: Make the theme for this app
+ThemeData mainTheme = ThemeData();
